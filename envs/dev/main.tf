2af7b99c90fedf220b377e8b120de84e7e2a5c84
Sreepathy H V <sreepathy.hv@hitachids.com>
2026-05-05 12:23:45 +0530

    Dev infra deployment with vm instance type updated again

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -43,7 +43,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "dev-vm"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
-  size                = "Standard_DS1_v2"
+  size                = "Standard_D2ls_v6"
 
   admin_username = "azureuser"
   admin_password = "Msois@123"
